de42ffd82dadec1e7feae4504e9a6efecc7c04ed
JosephOlabode <josepholabode17@gmail.com>
2022-07-12 09:48:22 -0700

    modified the swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,4 @@
-#include "main.h"
+include "main.h"
 
 /**
  * swap_int - Swap the value of two integers
@@ -9,7 +9,7 @@
  * Return: Nothing
  */
 
-void swap_int(int *a, int *b) 
+void swap_int(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
