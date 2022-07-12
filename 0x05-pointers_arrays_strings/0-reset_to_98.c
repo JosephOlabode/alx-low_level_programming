2d5c3ef13dbcde2b3f9ffc17d41d0efd9735cc3d
JosephOlabode <josepholabode17@gmail.com>
2022-07-12 08:45:12 -0700

    implemented the reassignment of variable name

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
+#include "main.h"
 
 /** 
  * Update the value of a pointer variable
  */
-void reset_to_98(int *n) {
-	*n = 98;
-}
 
 int main(void) {
 	int n;
@@ -15,4 +13,8 @@ int main(void) {
         reset_to_98(&n);
         printf("n=%d\n", n);
         return (0);
+}
+
+void reset_to_98(int *n) {
+	*n = 98;
 }
