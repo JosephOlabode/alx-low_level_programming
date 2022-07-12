2cb6d8e9e97602269a0d5a6a70505cf837d0b902
JosephOlabode <josepholabode17@gmail.com>
2022-07-12 09:56:58 -0700

    implemented the returning of length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: The character containing the string
+ *
+ * Return: The length of the character
+ */
+
+int _strlen(char *s)
+{
+	int len = sizeof(*s);
+	return (len);
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@ ARRAY FUNCTION AND POINTERS
 0-reset_to_98.c: Reset the value of n to 98
 
 1-swap.c: swap the value of the variables
+
+2-strlen.c: returning the lenght of a string
