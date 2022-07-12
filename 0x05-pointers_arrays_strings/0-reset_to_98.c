a3e305c7a49084dee3b01daeed79f6c035103881
JosephOlabode <josepholabode17@gmail.com>
2022-07-12 08:47:32 -0700

    corrected the code of task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -5,16 +5,6 @@
  * Update the value of a pointer variable
  */
 
-int main(void) {
-	int n;
-
-        n = 402;
-        printf("n=%d\n", n);
-        reset_to_98(&n);
-        printf("n=%d\n", n);
-        return (0);
-}
-
 void reset_to_98(int *n) {
 	*n = 98;
 }
